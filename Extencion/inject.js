c3f734df657aa1b8b309bc0e9e1c3eb1e0d4bb67
madmon20 <madmon18@gmail.com>
2016-05-19 15:26:25 +0300

    Add files via upload

--- Extencion/inject.js
+++ Extencion/inject.js
@@ -4,7 +4,12 @@ function init()
 	$(".watch-title").click(function(){
         console.log($(".watch-title").text());
     });
+	
+	
+	
 	console.log("IYAR - INIT");
+	console.log($(".comment-renderer-text-content:first").val());
+	
 }
 
 $(document).ready(function(){
