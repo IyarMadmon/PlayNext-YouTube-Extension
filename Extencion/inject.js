66ee95d71cb960284ba0b1dc72972b5d5e75f4ff
madmon20 <madmon18@gmail.com>
2016-05-19 15:41:27 +0300

    Update inject.js

--- Extencion/inject.js
+++ Extencion/inject.js
@@ -4,10 +4,8 @@ function init()
 	$(".watch-title").click(function(){
         console.log($(".watch-title").text());
     });
-	
 	console.log("IYAR - INIT");
 	console.log($(".comment-renderer-text-content:first").val());
-	
 }
 
 $(document).ready(function(){
