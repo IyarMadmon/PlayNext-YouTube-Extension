4b71c7d7254779dba1a227becb0c0b3eb2557562
IyarMadmon <madmon18@gmail.com>
2017-01-20 11:55:39 +0200

    move to modular
    works perfectly besides extra video button

--- ‏‏Extension/playNext.js
+++ ‏‏Extension/playNext.js
@@ -1,81 +1,106 @@
-var picHeight = 25;
-var picwidth = 25;
-var picClass = "TusCommentosPNButton";
-var enabledPicSrc = chrome.extension.getURL('images/PNEnabled.png');
-var disabledPicSrc = chrome.extension.getURL('images/PNDisabled.png');
-var picElements;
-var video;
+var engine = {
 
-$(document).ready(function(){
-	setTimeout(checkPlayerState, 1000);
-});
+  lastPageUrl: {},
+  video: null,
 
-function checkPlayerState() {
-	var dest = $("[isPicEnabled='true']").attr('id');
-	
-	if(isVideoPage() && (!$("." + picClass).length)) {
-		init();
-	} else if(dest != undefined && video && video.currentTime == video.duration) {
-		window.location="https://www.youtube.com" + dest;
-	}
-	
-	setTimeout(checkPlayerState, 1000);
-} // End of checkPlayerState
-
-function isVideoPage() {
+  start: function() {
+    lastPageUrl = {href: location.href, isLoaded: false};
+    this.checkPlayerState();
+  },
+  checkPlayerState: function () {
+    if(this.isVideoPage()) {
+      if(this.hasNavigated()) {
+        lastPageUrl = {href: location.href, isLoaded: true};
+        video = document.querySelector('video.video-stream');
+        setTimeout(buttons.init.bind(buttons), 800);
+        setTimeout(likePercentage.init.bind(likePercentage), 800);
+      } else if (this.hasVideoEnded()) {
+        var nextVideo;
+        if(nextVideo = buttons.nextVideo()) {
+          window.location="https://www.youtube.com" + nextVideo.getAttribute("id");
+        }
+      }
+    }
+    setTimeout(this.checkPlayerState.bind(this), 2000);
+  },
+  isVideoPage: function() {
     return location.href.indexOf("/watch") !== -1;
-} // End of isVideoPage
+  },
+  hasNavigated: function() {
+    return location.href !== lastPageUrl.href ||
+           !lastPageUrl.isLoaded;
+  },
+  hasVideoEnded: function() {
+    return video && video.currentTime == video.duration;
+  }
+};
 
-function init()
-{	
-	video = document.querySelector('video.video-stream');
-	loadPics();
-	setTimeout(addLikesPercentage, 600);
-	
-	$("#watch-more-related-button").click(function() {
-		setTimeout(loadPics, 1000);
-	});
-} // End of init
+var buttons = {
 
-function loadPics() {
-	$(".content-wrapper").each(function() {
-		if(!$(this).next().hasClass(picClass)) {
-			var el = $( '<div></div>' );
-			el.html(this.innerHTML);
-			var currHref = $('a', el).first().attr('href');
-			
-			var elem = $("<img></img>").
-					attr("src", disabledPicSrc).
-					attr("height", picHeight).
-					attr("width", picwidth).
-					attr("class", picClass).
-					attr("isPicEnabled", false).
-					attr("id", currHref);
-			$(this).after(elem);
-			initClickableButtons(elem);
-		}
-	});
-	
-} // End of loadPics
+  picHeight: 25,
+  picwidth: 25,
+  picClass: "PlayNextButton",
+  enabledPicSrc: chrome.extension.getURL('images/PNEnabled.png'),
+  disabledPicSrc: chrome.extension.getURL('images/PNDisabled.png'),
+  selectedButton: null,
 
-function initClickableButtons(elem) {
-	$(elem).click(function() {
-		if(this.getAttribute("isPicEnabled") == "false") {
-			$("[isPicEnabled='true']").attr('isPicEnabled',false).attr('src', disabledPicSrc);
-			this.src = enabledPicSrc
-			this.setAttribute("isPicEnabled", true);	
-		} else {
-			this.src = disabledPicSrc
-			this.setAttribute("isPicEnabled", false);	
-		}
-	});
-} // end of initClickableButtons
+  init: function() {
+    this.createButtons();
+    this.bindEvents();
+  },
 
-function addLikesPercentage() {
-	if($('#likesPercentage').length) return;
-	var calc = 0;
-	var numberoflikes = parseInt($('.like-button-renderer-like-button').find('span.yt-uix-button-content').html().replace(',', ''));
-	var numberofdislikes = parseInt($('.like-button-renderer-dislike-button').find('span.yt-uix-button-content').html().replace(',', ''));
-	if(numberoflikes !== 0 || numberofdislikes !== 0) { calc = (numberoflikes / (numberoflikes+numberofdislikes)) * 100;}
-	$('.like-button-renderer').before($("<span id='likesPercentage' style='color:#167ac6;'>"  +calc.toFixed(2)+ "%</span>"));
-}//End of addLikesPercentage
+  createButtons: function() {
+    console.log("createButtons", new Date().getTime());
+    $(".content-wrapper").each(function() {
+  		if(!$(this).next().hasClass(buttons.picClass)) {
+  			var el = $( '<div></div>' );
+  			el.html(this.innerHTML);
+  			var currHref = $('a', el).first().attr('href');
+  			var elem = $("<img></img>").
+  					attr("src", buttons.disabledPicSrc).
+  					attr("height", buttons.picHeight).
+  					attr("width", buttons.picwidth).
+  					attr("class", buttons.picClass).
+  					attr("id", currHref);
+  			$(this).after(elem);
+  		}
+    });
+  },
+  bindEvents: function() {
+    $("." + this.picClass).on("click", this.toggleButton);
+    $("#watch-more-related-button").click(function() {
+      console.log("more", new Date().getTime());
+  		setTimeout(buttons.createButtons, 1000);
+  	});
+  },
+  toggleButton: function () {
+    if(buttons.selectedButton === null) {
+      this.setAttribute("src", buttons.enabledPicSrc);
+      buttons.selectedButton = this;
+    } else if(buttons.selectedButton === this){
+      buttons.selectedButton = null;
+      this.setAttribute("src", buttons.disabledPicSrc);
+    } else {
+      buttons.selectedButton.setAttribute("src", buttons.disabledPicSrc);
+      this.setAttribute("src", buttons.enabledPicSrc);
+      buttons.selectedButton = this;
+    }
+  },
+  nextVideo: function() {
+    return this.selectedButton;
+  }
+};
+
+var likePercentage = {
+  init: function() {
+    var calc = 0;
+  	var numberoflikes = parseInt($('.like-button-renderer-like-button').find('span.yt-uix-button-content').html().split(',').join(''));
+  	var numberofdislikes = parseInt($('.like-button-renderer-dislike-button').find('span.yt-uix-button-content').html().split(',').join(''));
+  	if(numberoflikes !== 0 || numberofdislikes !== 0) {
+      calc = (numberoflikes / (numberoflikes+numberofdislikes)) * 100;
+    }
+  	$('.like-button-renderer').before($("<span style='color:#167ac6;'>"  +calc.toFixed(2)+ "%</span>"));
+  }
+};
+
+engine.start();
